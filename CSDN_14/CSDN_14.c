50afbd80eaffa627bfb6e4ba8fb29d611926be51
hunterqie666 <3063804025@qq.com>
2023-06-14 13:10:34 +0800

    CSDN博客代码

---
+++ CSDN_14/CSDN_14.c
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	printf("%d",10);
+//	return 0;
+//}
+
+//void main()
+//{
+//	printf("Hello World！");
+//}
+
+//void main()
+//{
+//	printf("Hello World！");
+//}
+
+
+//int main(void)//void表示main函数不接受参数
+//{
+//	printf("Hello World！");
+//	return 0;
+//}
+
+//int main(int argc,char* argv[])//指定main函数的参数
+//{
+//	printf("Hello World！");
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d", sizeof(double));
+//	return 0;
+//}
+
+//int main()
+//{
+//	int _a = 0;
+//	int 1 = 0;//错误
+//	int c = 0;
+//	int char = 1;//错误
+//	return 0;
+//}
+
+//int main()
+//{
+//	{
+//	int b = 10;
+//	}
+//	printf("%d", b);
+//	return 0;
+//}
+
+//int Add(int a, int b);
+//int Add(int a, int b)
+//{
+//	return a + b;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d %d", &a, &b);
+//	c = Add(a, b);
+//	printf("%d", c);
+//	return 0;
+//}
